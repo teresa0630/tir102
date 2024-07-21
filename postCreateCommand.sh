b638ea153a1e50730dfeeeb07277d306cb407178
teresa0630 <teresa910630@gmail.com>
2024-07-21 09:59:26 +0800

    Add files via upload

---
+++ postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+pip install pipenv \
+&& pipenv lock --dev \
+&& pipenv requirements --dev > requirements.txt \
+&& pip install -r requirements.txt
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
+&& sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
+
+
+git config --global --add safe.directory /workspaces/*
+
+zsh
